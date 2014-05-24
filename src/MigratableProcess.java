ec9c8fc63862f7c73f2ec8522b8ed169bbfc4c33
Pranav Saxena <psbits@gmail.com>
2014-05-24 16:43:57 -0400

    Migratable process interface and user console structure

---
+++ src/MigratableProcess.java
@@ -0,0 +1,15 @@
+
+import java.io.Serializable;
+
+
+ public interface MigratableProcess extends Serializable,Runnable {
+
+	static final long serialVersionUID = 1L;
+
+    	public void suspend();
+  
+    public void launch(String pname);
+    
+    public void remove();
+    
+}
---
+++ src/UserConsole.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class UserConsole {
+	public static void main(String args[]){
+		
+	}
+}
