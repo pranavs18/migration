52749c9e21f6540472313a6b299357ec7fcd49ad
Pranav Saxena <psbits@gmail.com>
2014-05-27 15:33:02 -0400

    GrepProcess - An example process

---
+++ src/GrepProcess.java
@@ -0,0 +1,96 @@
+import java.io.PrintStream;
+import java.io.EOFException;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.lang.Thread;
+import java.lang.InterruptedException;
+
+public class GrepProcess implements MigratableProcess
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private TransactionalFileInputStream  inFile;
+	private TransactionalFileOutputStream outFile;
+	private String query;
+
+	private volatile boolean suspending;
+	
+	public GrepProcess(){
+		
+	}
+
+	public GrepProcess(String args[]) throws Exception
+	{
+		if (args.length != 3) {
+			System.out.println("usage: GrepProcess <queryString> <inputFile> <outputFile>");
+			throw new Exception("Invalid Arguments");
+		}
+		
+		query = args[0];
+		inFile = new TransactionalFileInputStream(args[1]);
+		outFile = new TransactionalFileOutputStream(args[2], false);
+	}
+
+	public void run()
+	{ /*
+		PrintStream out = new PrintStream(outFile);
+	     DataInputStream in = new DataInputStream(inFile);
+
+		try {
+			while (!suspending) {
+				String line = in.readLine();
+
+				if (line == null) break;
+				
+				if (line.contains(query)) {
+					out.println(line);
+				}
+				
+				// Make grep take longer so that we don't require extremely large files for interesting results
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// ignore it
+				}
+			}
+		} catch (EOFException e) {
+			//End of File
+		} catch (IOException e) {
+			System.out.println ("GrepProcess: Error: " + e);
+		}
+
+
+		suspending = false;*/
+	}
+
+	public void suspend()
+	{
+		suspending = true;
+		while (suspending);
+	}
+
+	@Override
+	public void launch(String pname) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void launch(String pname, String Ipaddress, int portno) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
