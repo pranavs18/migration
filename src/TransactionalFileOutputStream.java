7a48927b9a64dc753f76a6299b997e2edfff6be2
Pranav Saxena <psbits@gmail.com>
2014-05-30 01:53:01 -0400

    writing bytes by bytes and seeking to a offset position while migrating the user process

--- src/TransactionalFileOutputStream.java
+++ src/TransactionalFileOutputStream.java
@@ -1,5 +1,7 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.io.Serializable;
 
 
@@ -8,16 +10,118 @@ public class TransactionalFileOutputStream extends OutputStream implements Seria
 	/**
 	 * 
 	 */
+    private transient RandomAccessFile raf = null;
 	private static final long serialVersionUID = 1L;
-
+    private boolean fileClosed;
+    private File file;
+	private int currentFileOffset;
+    
 	public TransactionalFileOutputStream(String string, boolean b) {
-		// TODO Auto-generated constructor stub
+		this.currentFileOffset=0;
+		this.file = new File(string);
+		this.fileClosed = b;
 	}
+
+	public TransactionalFileOutputStream(File file) {
+        this.file = file;
+        this.currentFileOffset = 0;
+    }
+	
+	@Override
+	public void write(byte[] bytes) {
+	
+		 try {
+	            raf = sendBytesOutputStream();
+	            try {
+					raf.write(bytes);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	            currentFileOffset+=bytes.length;
+	        } finally {
+	            if (raf != null) {
+	                try {
+						raf.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+	            }
+	        }
+	}
+	
+	private RandomAccessFile sendBytesOutputStream() {
+	        RandomAccessFile raf = null;
+			try {
+				raf = new RandomAccessFile(file, "rw");
+			    raf.seek(currentFileOffset);
+			} catch (IOException e) {
+		         e.printStackTrace();
+	         }
+	        return raf;
+	    }
+	
+	@Override
+    public void write(byte[] bytes, int fileOffset, int length) {
+        try {
+            raf = sendBytesOutputStream();
+            raf.write(bytes, fileOffset, length);
+            currentFileOffset+=length;
+            
+          }catch (Exception e) {
+	         e.printStackTrace();
+        }finally{
+        	if (raf != null) {
+                try {
+					raf.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				  }
+               }
+           }
+	   }
 
 	@Override
-	public void write(int arg0) throws IOException {
-		// TODO Auto-generated method stub
+	public void write(int b) {
+		 try {
+	            raf = sendBytesOutputStream();
+	            try {
+					raf.write(b);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	            currentFileOffset++;
+	        } finally {
+	            if (raf != null) {
+	                try {
+						raf.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+	            }
+	        }
 		
+	 }
+	
+	@Override
+    public void close() {
+        currentFileOffset = 0;
+        try {
+			super.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+
+	public boolean isFileClosed() {
+		return fileClosed;
 	}
 
+	public void setFileClosed(boolean fileClosed) {
+		this.fileClosed = fileClosed;
+	}
+	
+	
 }
+    
+
+
